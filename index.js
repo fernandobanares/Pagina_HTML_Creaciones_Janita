7b74dfab25237adf7527bc22200aec01fb837ccb
fernando.banares <fbanarescarrasco@gmail.com>
2022-05-24 16:24:47 -0400

    Entrega final

---
+++ index.js
@@ -0,0 +1,15 @@
+var fecha = "Lunes"
+let edad = 25
+const nombre = "Alex"
+
+console.log(nombre)
+
+let a = 10
+let b = 20
+
+let suma = a + b
+console.log(suma)
+
+let message = promt("Ingrese una letra")
+let resultado = entrada + " " + "ingresada"
+alert(resultado)
